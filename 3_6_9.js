76325f6aa07a123c424a39083c66875cb800e7e9
Rose Jang <jangmi749@gmail.com>
2023-01-21 22:48:01 +0900

    Lv.0) 369게임

--- 3_6_9.js
+++ 3_6_9.js
@@ -1,10 +1,10 @@
-function solution(order) {
+function solution1(order) {
   return (order + '').split('').filter((num) => num % 3 === 0 && num !== '0').length;
 }
 
 // * +정규표현식을 이용한 풀이
 
-function solution(order) {
+function solution2(order) {
   let answer = [...order.toString().matchAll(/[3|6|9]/g)].length;
   return answer;
 }
