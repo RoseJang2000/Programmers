f7818cace4806a66b908779670b8687280cfcf10
Rose Jang <jangmi749@gmail.com>
2023-01-21 22:23:36 +0900

    Lv.0) 369게임

---
+++ 3_6_9.js
@@ -0,0 +1,10 @@
+function solution(order) {
+  return (order + '').split('').filter((num) => num % 3 === 0 && num !== '0').length;
+}
+
+// * +정규표현식을 이용한 풀이
+
+function solution(order) {
+  let answer = [...order.toString().matchAll(/[3|6|9]/g)].length;
+  return answer;
+}
